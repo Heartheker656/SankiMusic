2945765edd41d6316b91a51030c2c2edab4529ee
Heartheker656 <152202029+Heartheker656@users.noreply.github.com>
2023-12-09 21:54:12 +0530

    Update thumbnails.py

--- SankiMusic/modules/utils/thumbnails.py
+++ SankiMusic/modules/utils/thumbnails.py
@@ -4,14 +4,9 @@ import textwrap
 
 import aiofiles
 import aiohttp
-import numpy as np
-import random
-
-from PIL import Image, ImageChops, ImageOps, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from youtubesearchpython.__future__ import VideosSearch
-
-from SankiMusic import bot
-from SankiMusic.resource import thumbs, colors
+from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
+                 ImageFont, ImageOps)
+from youtubesearchpython.future import VideosSearch
 from SankiMusic.utilities.config import YOUTUBE_IMG_URL
 
 
@@ -24,14 +19,6 @@ def changeImageSize(maxWidth, maxHeight, image):
     newImage = image.resize((newWidth, newHeight))
     return newImage
 
-
-def add_corners(im):
-    bigsize = (im.size[0] * 3, im.size[1] * 3)
-    mask = Image.new("L", bigsize, 0)
-    ImageDraw.Draw(mask).ellipse((0, 0) + bigsize, fill=255)
-    mask = mask.resize(im.size, Image.ANTIALIAS)
-    mask = ImageChops.darker(mask, im.split()[-1])
-    im.putalpha(mask)
 
 
 async def gen_thumb(videoid, user_id):
@@ -67,70 +54,27 @@ async def gen_thumb(videoid, user_id):
                     f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
                     await f.write(await resp.read())
                     await f.close()
-
-        try:
-            wxyz = await bot.get_profile_photos(user_id)
-            wxy = await bot.download_media(wxyz[0]['file_id'], file_name=f'{user_id}.jpg')
-        except:
-            abc = await bot.get_profile_photos(bot.id)
-            wxy = await bot.download_media(abc[0]['file_id'], file_name=f'{bot.id}.jpg')
-        xy = Image.open(wxy)
-        a = Image.new('L', [640, 640], 0)
-        b = ImageDraw.Draw(a)
-        b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
-        c = np.array(xy)
-        d = np.array(a)
-        e = np.dstack((c, d))
-        f = Image.fromarray(e)
-        x = f.resize((107, 107))
-
-        images = random.choice(thumbs)
-        border = random.choice(colors)
+       
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"SankiMusic/resource/{images}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        background = image2.filter(filter=ImageFilter.BoxBlur(0))
         enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.6)
-
-        image3 = changeImageSize(1280, 720, bg)
-        image5 = image3.convert("RGBA")
-        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
-
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.ANTIALIAS)
-        logo.save(f"cache/chop{videoid}.png")
-        if not os.path.isfile(f"cache/cropped{videoid}.png"):
-            im = Image.open(f"cache/chop{videoid}.png").convert("RGBA")
-            add_corners(im)
-            im.save(f"cache/cropped{videoid}.png")
-
-        crop_img = Image.open(f"cache/cropped{videoid}.png")
-        logo = crop_img.convert("RGBA")
-        logo.thumbnail((365, 365), Image.ANTIALIAS)
-        width = int((1280 - 365) / 2)
-        background = Image.open(f"cache/temp{videoid}.png")
-        background.paste(logo, (width + 2, 138), mask=logo)
-        background.paste(x, (710, 427), mask=x)
-        background.paste(image3, (0, 0), mask=image3)
-        img = ImageOps.expand(background, border=10, fill=f"{border}")
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("SankiMusic/resource/font2.ttf", 45)
-        ImageFont.truetype("SankiMusic/resource/font2.ttf", 70)
-        arial = ImageFont.truetype("SankiMusic/resource/font2.ttf", 30)
-        ImageFont.truetype("SankiMusic/resource/font.ttf", 30)
-        para = textwrap.wrap(title, width=32)
-        try:
+        background = enhancer.enhance(1.0)       
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        font2 = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        arial = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        name_font = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        para = textwrap.wrap(title, width=2)
+        j = 0
+        draw.text(
+            (5, 5), f"", fill="white", font=name_font
+        )
+           
             draw.text(
                 (450, 35),
-                f"STARTED PLAYING",
+                f"",
                 fill="white",
                 stroke_width=1,
                 stroke_fill="white",
@@ -161,10 +105,10 @@ async def gen_thumb(videoid, user_id):
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
             ((1280 - text_w) / 2, 665),
-            f"Duration: {duration} Mins",
+            f"",
             fill="white",
             font=arial,
-        )
+    )
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
@@ -210,111 +154,75 @@ async def que_thumb(videoid, user_id):
                     await f.write(await resp.read())
                     await f.close()
 
-        try:
-            wxyz = await bot.get_profile_photos(user_id)
-            wxy = await bot.download_media(wxyz[0]['file_id'], file_name=f'{user_id}.jpg')
-        except:
-            abc = await bot.get_profile_photos(bot.id)
-            wxy = await bot.download_media(abc[0]['file_id'], file_name=f'{bot.id}.jpg')
-        xy = Image.open(wxy)
-        a = Image.new('L', [640, 640], 0)
-        b = ImageDraw.Draw(a)
-        b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
-        c = np.array(xy)
-        d = np.array(a)
-        e = np.dstack((c, d))
-        f = Image.fromarray(e)
-        x = f.resize((107, 107))
-
-        images = random.choice(thumbs)
-        border = random.choice(colors)
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"SankiMusic/resource/{images}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        background = image2.filter(filter=ImageFilter.BoxBlur(0))
         enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.6)
-
-        image3 = changeImageSize(1280, 720, bg)
-        image5 = image3.convert("RGBA")
-        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
-
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.ANTIALIAS)
-        logo.save(f"cache/chop{videoid}.png")
-        if not os.path.isfile(f"cache/cropped{videoid}.png"):
-            im = Image.open(f"cache/chop{videoid}.png").convert("RGBA")
-            add_corners(im)
-            im.save(f"cache/cropped{videoid}.png")
-
-        crop_img = Image.open(f"cache/cropped{videoid}.png")
-        logo = crop_img.convert("RGBA")
-        logo.thumbnail((365, 365), Image.ANTIALIAS)
-        width = int((1280 - 365) / 2)
-        background = Image.open(f"cache/temp{videoid}.png")
-        background.paste(logo, (width + 2, 138), mask=logo)
-        background.paste(x, (710, 427), mask=x)
-        background.paste(image3, (0, 0), mask=image3)
-        img = ImageOps.expand(background, border=10, fill=f"{border}")
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("SankiMusic/resource/font2.ttf", 45)
-        ImageFont.truetype("SankiMusic/resource/font2.ttf", 70)
-        arial = ImageFont.truetype("SankiMusic/resource/font2.ttf", 30)
-        ImageFont.truetype("SankiMusic/resource/font.ttf", 30)
-        para = textwrap.wrap(title, width=32)
-        try:
-            draw.text(
-                (455, 35),
-                "ADDED TO QUEUE",
-                fill="white",
-                stroke_width=1,
-                stroke_fill="white",
-                font=font,
-            )
-            if para[0]:
-                text_w, text_h = draw.textsize(f"{para[0]}", font=font)
-                draw.text(
-                    ((1280 - text_w) / 2, 560),
-                    f"{para[0]}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if para[1]:
-                text_w, text_h = draw.textsize(f"{para[1]}", font=font)
-                draw.text(
-                    ((1280 - text_w) / 2, 610),
-                    f"{para[1]}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-        except:
-            pass
-        text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
+        background = enhancer.enhance(1.0)       
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        font2 = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        arial = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        name_font = ImageFont.truetype("SankiMusic/resource/font2.ttf", 0)
+        para = textwrap.wrap(title, width=2)
+        j = 0
         draw.text(
-            ((1280 - text_w) / 2, 665),
-            f"Duration: {duration} Mins",
+            (5, 5), f"", fill="white", font=name_font
+        )
+        draw.text(
+            (600, 150),
+            "",
             fill="white",
+            stroke_width=2,
+            stroke_fill="white",
+            font=font2,
+        )
+        for line in para:
+            if j == 1:
+                j += 1
+                draw.text(
+                    (600, 340),
+                    f"{line}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="white",
+                    font=font,
+                )
+            if j == 0:
+                j += 1
+                draw.text(
+                    (600, 280),
+                    f"{line}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="white",
+                    font=font,
+                )
+
+        draw.text(
+            (600, 450),
+            f"",
+            (255, 255, 255),
             font=arial,
         )
-
+        draw.text(
+            (600, 500),
+            f"",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (600, 550),
+            f"",
+            (255, 255, 255),
+            font=arial,
+        )
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
             pass
-        file = f"cache/que{videoid}_{user_id}.png"
-        img.save(f"cache/que{videoid}_{user_id}.png")
-        return f"cache/que{videoid}_{user_id}.png"
-    except Exception as e:
-        print(e)
+        background.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
+    except Exception:
         return YOUTUBE_IMG_URL
